c35f25db13a88fe49713edf02b1206b9d76bce9f
Mateus F. Cunha <101605976+MateusFranca@users.noreply.github.com>
2023-10-12 19:54:29 -0300

    Refatorando o código e padronizando os métodos

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+    //Criamos um atributo para o nosso controlador
+    public function __construct(Marca $marca) {
+        $this->marca = $marca;
+
+    }
     /**
      * Display a listing of the resource.
      *
@@ -16,7 +21,10 @@ class MarcaController extends Controller
     // Retorna todos os registros da Marca
     public function index()
     {
-        $marcas = Marca::all();
+        //$marcas = Marca::all();
+        
+        // Estamos acessando um método de um objeto
+        $marcas = $this->marca->all();
         return $marcas;
     }
 
@@ -36,25 +44,29 @@ class MarcaController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+     //Método stores respondavel por criar os novos dados no BD
     public function store(Request $request)
     {   
         //Recuperamos o Model "Marca" e estamos criando um novo registo. Por causa do all() a inserção é em massa o que faz com que precisamos no Model "Marca" ele possa receber os dados dessa forma. 
-        $marca = Marca::create($request->all());
+        //$marca = Marca::create($request->all());
         //O dd e para mandar a requisição e all() para isolar quando vamos ver na postman
         //dd($request->all());
+        $marca = $this->marca->create($request->all());
         return $marca;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
     
      // Retorna apena um registro da Marca, temos que passar por paramentro a identificação desse respetivo registro. 
-    public function show(Marca $marca)
+    public function show($id)
     {
+        $marca = $this->marca->find($id);
         return $marca;
     }
 
@@ -73,18 +85,20 @@ class MarcaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
 
      //Método update serve para atualizar os dados
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
         /*
         print_r($request->all()); // os dados atualizados
         echo "<hr>";
         print_r($marca->getAttributes()); // os dados antigos
         */
+        //$marca->update($request->all());
+        $marca = $this->marca->find($id);
         $marca->update($request->all());
         return $marca; 
     }
@@ -92,12 +106,13 @@ class MarcaController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
         //print_r($marca->getAttributes());
+        $marca = $this->marca->find($id);
         $marca->delete();
         return ['msg' => 'A marca foi removido com sucesso!'];
     }
