1743d0a0abf3068ea1599125b0c7aa3bd80b2f3b
Mateus F. Cunha <101605976+MateusFranca@users.noreply.github.com>
2023-10-12 19:56:42 -0300

    removendo comentários

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Marca; //Importar o Model "Marca" para podermos usar ele aqui.
+use App\Models\Marca;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
-    //Criamos um atributo para o nosso controlador
     public function __construct(Marca $marca) {
         $this->marca = $marca;
 
@@ -18,12 +17,8 @@ class MarcaController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    // Retorna todos os registros da Marca
     public function index()
     {
-        //$marcas = Marca::all();
-        
-        // Estamos acessando um método de um objeto
         $marcas = $this->marca->all();
         return $marcas;
     }
@@ -45,13 +40,8 @@ class MarcaController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-     //Método stores respondavel por criar os novos dados no BD
     public function store(Request $request)
     {   
-        //Recuperamos o Model "Marca" e estamos criando um novo registo. Por causa do all() a inserção é em massa o que faz com que precisamos no Model "Marca" ele possa receber os dados dessa forma. 
-        //$marca = Marca::create($request->all());
-        //O dd e para mandar a requisição e all() para isolar quando vamos ver na postman
-        //dd($request->all());
         $marca = $this->marca->create($request->all());
         return $marca;
     }
@@ -63,7 +53,6 @@ class MarcaController extends Controller
      * @return \Illuminate\Http\Response
      */
     
-     // Retorna apena um registro da Marca, temos que passar por paramentro a identificação desse respetivo registro. 
     public function show($id)
     {
         $marca = $this->marca->find($id);
@@ -89,15 +78,8 @@ class MarcaController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-     //Método update serve para atualizar os dados
     public function update(Request $request, $id)
     {
-        /*
-        print_r($request->all()); // os dados atualizados
-        echo "<hr>";
-        print_r($marca->getAttributes()); // os dados antigos
-        */
-        //$marca->update($request->all());
         $marca = $this->marca->find($id);
         $marca->update($request->all());
         return $marca; 
@@ -111,7 +93,6 @@ class MarcaController extends Controller
      */
     public function destroy($id)
     {
-        //print_r($marca->getAttributes());
         $marca = $this->marca->find($id);
         $marca->delete();
         return ['msg' => 'A marca foi removido com sucesso!'];
