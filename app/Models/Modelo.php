5da16f2fd319733b7c552cbbabbd7fcc22fde018
Mateus F. Cunha <101605976+MateusFranca@users.noreply.github.com>
2023-10-13 16:16:22 -0300

    CRUD - table modelos

--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Modelo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['marca_id', 'nome', 'imagem', 'numero_portas', 'lugares', 'air_bag', 'abs'];
+
+    public function rules() {
+        return [
+            'marca_id' => 'exists:marcas,id',
+            'nome' => 'required|unique:modelos,nome,'.$this->id.'|min:3',
+            'imagem' => 'required|file|mimes:png,jpeg,jpg',
+            'numero_portas' => 'required|integer|digits_between:1,5', //(1,2,3,4,5)
+            'lugares' => 'required|integer|digits_between:1,20',
+            'air_bag' => 'required|boolean',
+            'abs' => 'required|boolean' //true, false, 1, 0, "1", "0"
+        ];
+    }
+
+    public function feedback() {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'O nome da marca já exite',
+            'nome.min' => 'O nome deve ter no mínimo 3 caracteres',
+            'imagem.mimes' => 'O arquivo deve ser uma imagem png',
+            'integer' => 'O campo :attribute é possível preencher apenas números',
+            'boolean' => 'O campo :attribute é obrigatório e deve ser verdadeiro (1) ou falso (0). '
+        ];
+    }
 }
